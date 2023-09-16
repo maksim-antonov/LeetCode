422c89cc7cd87d111385a2f684306039bc382a4f
Maksim Antonov <67494903+maksim-antonov@users.noreply.github.com>
2023-09-16 20:40:11 +0200

    Time: 47 ms (98.98%), Space: 44 MB (91.59%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -5,16 +5,21 @@
 const removeDuplicates = (nums) => {
     if (nums.length === 0) return 0;
 
-    let k = 0;
-    let i = 1;
+    let i = 0;
+    let k = 1;
 
     while (i < nums.length) {
-        if (nums[i] !== nums[k]) {
-            nums[++k] = nums[i]
+        if (nums[i] === nums[k-1]) {
+            i++;
+            continue;
         }
 
+        // Copy it to the k-th position in the array
+        nums[k] = nums[i];
+        // Increment k and i by one
+        k++;
         i++;
     }
 
-    return k + 1;
+    return k;
 };
