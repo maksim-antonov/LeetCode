f4d72c58b337b3cd4de92f541f651e92b88116bb
Maksim Antonov <67494903+maksim-antonov@users.noreply.github.com>
2023-09-16 21:12:14 +0200

    Time: 53 ms (56.06%), Space: 42.3 MB (14.5%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let j = 0;
+
+    for (let n of nums) {
+        if (n !== val) {
+            nums[j++] = n
+        }
+    }
+
+    return j;
+};
